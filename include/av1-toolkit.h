8e6a7881b714b3f6bf8e79f8e847b94edcd796fc
Mario M Sarria Jr <mars@MarsyMPX.local>
2025-09-30 22:56:55 -0700

    initial commit

---
+++ include/av1-toolkit.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+
+inline void say_hello() {
+    std::cout << "Hello from header function!\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my_header.h"
+
+int main() {
+    std::cout << "Hello from C++23 project!\n";
+    say_hello();
+    return 0;
+}
